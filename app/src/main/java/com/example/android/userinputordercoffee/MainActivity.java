0fd2a79939f0870c135c4dd9b8083579fcf9f8de
macroAtom <1650556387@qq.com>
2021-11-26 14:51:56 +0800

    当前可以正常运行

---
+++ app/src/main/java/com/example/android/userinputordercoffee/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.userinputordercoffee;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ *
+ */
+public class MainActivity extends Activity {
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_a);
+    }
+}
