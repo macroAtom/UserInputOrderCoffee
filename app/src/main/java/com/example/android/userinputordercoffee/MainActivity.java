873fccdb8a66872224c9681bf70ba8432ff624e8
macroAtom <1650556387@qq.com>
2021-11-27 11:50:57 +0800

    1.style 修改 2.价格和数量的初始化 3.本地化 4.hard code 进行了配置化

--- app/src/main/java/com/example/android/userinputordercoffee/MainActivity.java
+++ app/src/main/java/com/example/android/userinputordercoffee/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.android.userinputordercoffee;
 
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,22 +10,48 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
 
 /**
  *
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     /**
      * This method will auto run when app is launcher
      *
      * @param bundle
      */
 
-    int quantity = 2;
+    int quantity;
+
+    private void getQuantity() {
+        try {
+            quantity = Integer.parseInt(getString(R.string.initial_quantity));
+        } catch (NumberFormatException nfe) {
+            System.out.println("Could not parse " + nfe);
+        }
+    }
+
+    ;
+    /**
+     * Get the currency format for the user-selected locale.
+     * 获取本地的货币格式
+     */
+    private final NumberFormat mCurrencyFormat = NumberFormat.getCurrencyInstance();
 
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_a);
+        /**
+         * 初始化数量和价格
+         */
+        getQuantity();
+        TextView abc = (TextView) findViewById(R.id.order_summary_text_view);
+        int price = quantity * 5;
+        String initialPrice = String.valueOf(price);
+        abc.setText(initialPrice);
     }
 
     /**
@@ -64,71 +88,77 @@ public class MainActivity extends Activity {
         TextView orderView = (TextView) findViewById(R.id.order_summary_text_view);
         orderView.setText(priceMessage);
 
-//        发送邮件
-//        String[] emailTo = {"abcd@126.com"};
-//        composeEmail(emailTo,"Intent Text Subject","Hello 你们好吗");
+//        发送邮件,邮箱为虚假邮箱
+        String[] emailTo = {"abcd@126.com"};
+        composeEmail(emailTo, "Intent Text Subject", priceMessage);
     }
 
     /**
      * 计算订单总价格
+     *
      * @return
      */
-    private int calculatePrice(boolean isOrderWhipped,boolean isOrderChocolate){
+    private String calculatePrice(boolean isOrderWhipped, boolean isOrderChocolate) {
         int price = 5;
-        if(isOrderWhipped){
-            price +=1;
+        if (isOrderWhipped) {
+            price += 1;
         }
-        if(isOrderChocolate){
-            price +=2;
+        if (isOrderChocolate) {
+            price += 2;
         }
-        int totalPrice = quantity * price;
-        Log.i("MainActivity", "calculatePrice: totalPrice: "+totalPrice+"\tquantitiy:"+quantity);
+        price = quantity * price;
+
+        String totalPrice = mCurrencyFormat.format(price);
+        Log.i("MainActivity", "calculatePrice: totalPrice: " + totalPrice + "\tquantitiy:" + quantity);
         return totalPrice;
     }
 
     /**
      * This method shows message on the device screen.
+     *
      * @return
      */
     @NonNull
-    private String displayMessage(){
+    private String displayMessage() {
 
 
         CheckBox whippedCream = findViewById(R.id.whipped_cream_checkbox);
 
         CheckBox chocolate = findViewById(R.id.chocolate_checkbox);
 
-        boolean isOrderWhipped=whippedCream.isChecked();
-        boolean isOrderChocolate=chocolate.isChecked();
+        boolean isOrderWhipped = whippedCream.isChecked();
+        boolean isOrderChocolate = chocolate.isChecked();
 
         EditText nameView = (EditText) findViewById(R.id.name_edit_text_view);
         String name = nameView.getText().toString();
-        int price = calculatePrice(isOrderWhipped,isOrderChocolate);
+        String price = calculatePrice(isOrderWhipped, isOrderChocolate);
 
-        String priceMessage = "Name: " + name;
-        priceMessage += "\nadd whippedCream? " + isOrderWhipped;
-        priceMessage += "\nadd chocolate? " + isOrderChocolate;
-        priceMessage += "\nToTal:" + price;
+        String priceMessage = getString(R.string.hint_name) + ": " + name;
+        Log.i("MainActivity", "getString(R.string.hint_name) " + getString(R.string.hint_name) + " getString(R.string.hint_name,name)");
+        priceMessage += "\n" + getString(R.string.whipped_cream) + "? " + isOrderWhipped;
+        priceMessage += "\n" + getString(R.string.chocolate) + "? " + isOrderChocolate;
+        priceMessage += "\n" + getString(R.string.total_price) + ": " + price;
         priceMessage += "\nThank you!";
 
         return priceMessage;
     }
 
-//    /**
-//     * 通过Intent调用其他app
-//     * @param addresses
-//     * @param subject
-//     * @param text
-//     */
-//    public void composeEmail(String[] addresses, String subject, String text) {
-//        Intent intent = new Intent(Intent.ACTION_SENDTO);
-//        intent.setType("*/*");
-//        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
-//        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-//        intent.putExtra(Intent.EXTRA_TEXT,text);
-//        if (intent.resolveActivity(getPackageManager()) != null) {
-//            startActivity(intent);
-//        }
-//    }
+    /**
+     * 通过Intent调用其他app
+     *
+     * @param addresses
+     * @param subject
+     * @param text
+     */
+    public void composeEmail(String[] addresses, String subject, String text) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 
 }
